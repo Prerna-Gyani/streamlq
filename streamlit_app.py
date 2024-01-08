0e396399c1f5906257f5566561e6af22868b2e08
Prerna-Gyani <142766837+Prerna-Gyani@users.noreply.github.com>
2024-01-09 01:12:51 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,26 @@
 import streamlit as st
+import os
+from constants import APIKEY
+from langchain.document_loaders import DirectoryLoader
+from langchain.indexes import VectorstoreIndexCreator
+from langchain.llms import OpenAI
+from langchain.chat_models import ChatOpenAI
 
-st.title('🎈 App Name')
+os.environ['OPENAI_API_KEY'] = "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"
 
-st.write('Hello world!')
+# App Framework
+st.title("🧠 Rudimentary Second Brain")
+prompt = st.text_input('Plug in your prompt here.')
+
+import streamlit as st
+
+# Using object notation
+add_selectbox = st.sidebar.markdown("# Welcome!")
+
+
+if prompt:
+    loader = DirectoryLoader("Resources", glob='*')
+    index = VectorstoreIndexCreator().from_loaders([loader])
+    st.markdown("## Results: ")
+    st.write(index.query(prompt))
+    #st.write(index.query(prompt, llm=ChatOpenAI()))
